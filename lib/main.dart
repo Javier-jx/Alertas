a950a4405444493eda42ce64052fa0308d71b51a
Javier Diaz <javierdiazjxp28@gmail.com>
2021-04-29 11:52:56 -0500

    Practica departemental

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_widget/pages/home.page.dart';
+
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      home:  HomePages() ,
+      
+     );
+
+  }
+}
