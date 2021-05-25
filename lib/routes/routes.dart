722afd931d3fec073b97882e726fd0af568b3f75
Javier Diaz <javierdiazjxp28@gmail.com>
2021-05-25 12:45:23 -0500

    Alertas

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -3,13 +3,15 @@ import 'package:flutter_application_widget/pages/alert_page.dart';
 import 'package:flutter_application_widget/pages/avatar_page.dart';
 import 'package:flutter_application_widget/pages/card_page.dart';
 import 'package:flutter_application_widget/pages/home_page.dart';
-
+import 'package:flutter_application_widget/pages/guardar_page.dart';
 
 Map <String, WidgetBuilder> getRoutes(){
-  return <String, WidgetBuilder>{
+  var map = <String, WidgetBuilder>{
   '/': (BuildContext context) => HomePage(),
   'alert': (BuildContext context) => AlertPage(),
   'avatar': (BuildContext context) => AvatarPage(),
   'card': (BuildContext context) => CardPage(),
+  
   };
+  return map;
 }
