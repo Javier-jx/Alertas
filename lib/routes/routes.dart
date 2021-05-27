ab584293401709c0713e14099fd0128aba866a12
Javier Diaz <javierdiazjxp28@gmail.com>
2021-05-27 10:05:43 -0500

    Implementacion de Avatares

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_widget/pages/alert_page.dart';
 import 'package:flutter_application_widget/pages/avatar_page.dart';
+import 'package:flutter_application_widget/pages/avatars/circuleAvatar.dart';
+import 'package:flutter_application_widget/pages/avatars/circuleConBorder.dart';
+import 'package:flutter_application_widget/pages/avatars/regtangular.dart';
+import 'package:flutter_application_widget/pages/avatars/regtangularBorde.dart';
 import 'package:flutter_application_widget/pages/card_page.dart';
 import 'package:flutter_application_widget/pages/home_page.dart';
-import 'package:flutter_application_widget/pages/guardar_page.dart';
+
+
 
 Map <String, WidgetBuilder> getRoutes(){
   var map = <String, WidgetBuilder>{
@@ -11,6 +16,11 @@ Map <String, WidgetBuilder> getRoutes(){
   'alert': (BuildContext context) => AlertPage(),
   'avatar': (BuildContext context) => AvatarPage(),
   'card': (BuildContext context) => CardPage(),
+  
+  'avatar/circular':(BuildContext context)=>CircularAvatar(),
+        'avatar/circularBorde':(BuildContext context)=>CircularBordeAvatar(),
+        'avatar/rectangulo':(BuildContext context)=>RectanguloAvatar(),
+        'avatar/rectanguloBorde':(BuildContext context)=>RectanguloBordeAvatar(), 
   
   };
   return map;
